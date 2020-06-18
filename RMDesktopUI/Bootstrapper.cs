c98879a9e6e28dd81a4292f3a4aefbd15df01e0f
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-06-18 19:23:36 +1000

    Add WPF to the project and applied MVVM. Used Caliburn for WPF

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //on start up display Shell view model as our start up
+            //ViewModel will load view based on caliburn wiring those two together
+
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
