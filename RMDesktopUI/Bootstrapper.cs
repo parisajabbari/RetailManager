948e2dc19eb31735c9be3d89e5637b1620a3445d
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-06-19 15:35:27 +1000

    Include simple container DI system

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -11,17 +11,48 @@ namespace RMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             //on start up display Shell view model as our start up
             //ViewModel will load view based on caliburn wiring those two together
-
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
