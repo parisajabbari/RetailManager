2fb859ae3cefc520a9ab8ec55f2c306e31721b62
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-06-24 15:50:08 +1000

    Create WPF Login form
    Added password box helper from a stackoverflow solution

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginViewModel;
+        public ShellViewModel( LoginViewModel loginViewModel )
+        {
+            _loginViewModel = loginViewModel;
+
+            //Immediatelly activate login in our ShellViewModel
+            ActivateItem(_loginViewModel);
+        }  
     }
 }
